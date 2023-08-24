daf668d197b323af547e941bfacc2c8f35ebbe3f
rmelton3 <123668403+rmelton3@users.noreply.github.com>
2023-08-24 17:54:50 -0500

    added name and major

--- hello.c
+++ hello.c
@@ -6,6 +6,8 @@ int main(int argc, char **argv) {
   printf("Broadcasting, media production\n");
   printf("hello Chloe!\n");
 
+  printf("Hey, I'm Riley!\n");
+  printf("I am an electrical engineer major\n");
   return 0;
 }
 
